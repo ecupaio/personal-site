5e8b4064ce76302328790fb76f418eb8291f6c55
Ed Cupaioli <cupaiolie@dnc.org>
2017-04-19 15:42:12 -0400

    removed iframe function

--- _includes/js/site-functions.js
+++ _includes/js/site-functions.js
@@ -41,30 +41,7 @@ $(document).ready(function() {
     });
 
     //Load Iframe
-    $('.iframe-overlay').click(function() {
-        var iframeURL = $('.load-iframe').data('url');
-        $('.iframe-window').attr('src',iframeURL);
-        $('.load-iframe i').attr('class','fa fa-circle-o-notch fa-spin');
-        $('.iframe-window').load(function(){
-            $('.iframe-overlay').addClass('hide');
-            $('#project-iframe').attr('style','');
-            $('.iframe-window').addClass('active');
-            $('.iframe-buttons').slideDown();
-        });
-    });
-    $('.resize').click(function(){
-        if ($('.enlarge').hasClass('active')) {
-            $('.enlarge').removeClass('active');
-            $('.minimize').addClass('active');
-        } else {
-            $('.enlarge').addClass('active');
-            $('.minimize').removeClass('active');
-        }
-        $('#project-iframe, #project-body').toggleClass('active');
-        $('html,body').delay('500').animate({
-            scrollTop: $('#project-iframe').offset().top
-        });
-    });
+    
     $('.slider').slick({
         dots: true,
         autoplay: true,
