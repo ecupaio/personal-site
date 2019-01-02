9296a6abf54e43ec31c212015a0556c3337ca57a
ecupaio <ed.cupaioli@arcadiapower.com>
2019-01-02 17:04:23 -0500

    basic layout

--- js/site-functions.js
+++ js/site-functions.js
@@ -8,5 +8,26 @@ $(document).ready(function() {
   setTimeout(function() {
     $('#home .hero-text').addClass('active');
   }, 1000);
-
+  //project
+  $('.project-col').click(function(e) {
+    var projectTitle = $(this).find('.project-title').text();
+    var projectLink = $(this).find('.project-link').text();
+    var projectTech = $(this).find('.project-tech').text();
+    var projectImages = JSON.parse($(this).find('.project-images').text());
+    $.each(projectImages,function(i,img) {
+      $('.project-slider').append("<div class='slide'><img src='/images/"+img+"' /></div>");
+    });
+    $('.project-overlay').addClass('active');
+  });
+  //contact form
+  $('.form-input input').focus(function() {
+    if ($(this).val() === '') {
+      $(this).parent().find('.placeholder').addClass('active');
+    }
+  });
+  $('.form-input input').focusout(function() {
+    if ($(this).val() === '') {
+      $(this).parent().find('.placeholder').removeClass('active');
+    }
+  });
 });
