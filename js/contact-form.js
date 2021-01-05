fcae5d2b92e15ea6248355858c1ebd35607d57df
Ed Cupaioli <ecupaio@gmail.com>
2021-01-05 09:14:08 -0800

    fixed contact form

--- js/contact-form.js
+++ js/contact-form.js
@@ -2,23 +2,11 @@ $(function() {
   $('#contact-form').submit(function(e) {
     e.preventDefault();
     $.ajax({
-    url: 'https://docs.google.com/forms/d/10kXaHPdQGi2R-hnklYeAw9jtW67vCu9Z1ALC6AdyzoM/formResponse',     //The public Google Form url, but replace /view with /formResponse
-    data: $(this).serialize(),
-    type: 'POST',
-    statusCode: {
-      0: function(data) {
-        //success
-        $('#form-success').text('hooray!');
-      },
-      200: function(data) {
-        //success
-        $('#form-success').text('hooray!');
-      },
-      403: function(data) {
-        //error
-        alert('Oh no! something went wrong. we should check our code to make sure everything matches with Google');
-      }
-    }
-  });
+      url: 'https://docs.google.com/forms/d/10kXaHPdQGi2R-hnklYeAw9jtW67vCu9Z1ALC6AdyzoM/formResponse',     //The public Google Form url, but replace /view with /formResponse
+      data: $(this).serialize(),
+      type: 'POST'
+    }).always(function(data) {
+      $('.form-success').addClass('active');
+    });
   });
 });
