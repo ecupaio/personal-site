963cbb706644e12b04381c3e429bd2fc108a57a7
Ed Cupaioli <ecupaio@gmail.com>
2021-07-12 11:35:51 -0700

    updated CSS workflow

--- gruntfile.js
+++ gruntfile.js
@@ -11,18 +11,24 @@ module.exports = function(grunt) {
         }]
       }
     },
+    
     watch: {
       imagemin: {
-        files: 'img/*.{png,jpg,gif}',
-        tasks: ['imagemin']
+        files: 'img/*.{png,jpg,gif,jpeg,svg}',
+        tasks: ['newer:imagemin:target']
       },
-      uglify: {
+      terser: {
         files: ['js/*.js',"js/min/!*.min.js"],
-        tasks: ['newer:uglify:target']
+        tasks: ['newer:terser:target']
+      },
+      css: {
+        files: ['scss/*.scss'],
+        tasks: ['sass', 'postcss']
       }
     },
-    uglify: {
+    terser: {
       options: {
+        compress: true,
         mangle: false
       },
       target: {
@@ -35,24 +41,58 @@ module.exports = function(grunt) {
         }]
       }
     },
+    sass: {
+      options: {
+        compress: false
+      },
+      scss: {
+        files: [{
+          expand: true,
+          cwd: 'scss/',
+          src: ['*.scss','!_*.scss'],
+          dest: 'css/',
+          ext: '.min.css'
+        }]
+      }    
+    },
+    postcss: {
+      options: {
+        processors: [
+          require('autoprefixer')({overrideBrowserslist: ['last 2 versions']}),
+          require('csswring')()
+        ]
+      },
+      mincss: {
+        files: [{
+          expand: true,
+          cwd: 'css/',
+          src: ['*.min.css'],
+          dest: 'css/'
+        }]
+      }
+    },
     shell: {
-      
       jekyllServe: {
         command: 'bundle exec jekyll serve --livereload '
       }
     },
 		concurrent: {
-			target: ['shell:jekyllServe','watch'],
+      target: ['shell:jekyllServe','watch'],
 			options: {
 				logConcurrentOutput: true
 			}
 		}
   });
+  grunt.loadNpmTasks('grunt-autoprefixer');
+  grunt.loadNpmTasks('grunt-contrib-sass'); 
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-postcss');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-imagemin');
   grunt.loadNpmTasks('grunt-newer');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-terser');
 	grunt.loadNpmTasks('grunt-concurrent');
   grunt.loadNpmTasks('grunt-shell');
-  grunt.registerTask('default', ['uglify','concurrent:target']);
+  grunt.loadNpmTasks('grunt-cwebp');
+  grunt.registerTask('default', ['terser','sass', 'postcss','concurrent:target']);
 };
