155401f5c5fe1d68ee90688fb4c2a95e65f17f5c
Ed Cupaioli <ecupaio@gmail.com>
2021-07-26 17:15:43 -0700

    added script for blog

---
+++ js/blog.js
@@ -0,0 +1,21 @@
+$(function() {
+  var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = window.location.search.substring(1),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return typeof sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+        }
+    }
+    return false;
+};
+  if (window.location.search.indexOf('tag') > -1) {
+    var tag = getUrlParameter('tag');
+    
+  }
+});
