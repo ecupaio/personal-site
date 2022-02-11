6cefaf14cdbb1e9c376ef73ce0a49813bca7d31e
Ed Cupaioli <ecupaio@gmail.com>
2022-02-11 14:17:48 -0800

    added sub menus

--- js/site-functions.js
+++ js/site-functions.js
@@ -13,54 +13,6 @@ $(document).ready(function() {
       scrollTop(selector);
     }
   });
-
-  //menu
-  $("#nav-toggle").click(function () {
-    $(".project-overlay, body").removeClass("active");
-    $("#nav-toggle, #sidebar-nav, main").toggleClass("active");
-    if ($("#sidebar-nav").hasClass("active")) {
-      $("#sidebar-nav .nav-link").each(function (i) {
-        var $this = $(this);
-        setTimeout(function () {
-          $this.addClass("active");
-        }, i * 100);
-      });
-    } else {
-      $("#sidebar-nav .nav-link").removeClass("active");
-    }
-  });
-  $(".nav-link").click(function (e) {
-    $("#nav-toggle, #sidebar-nav, main").removeClass("active");
-  });
-  //on scroll top sections
-
-  $(window).scroll(function () {
-    function elementScrolled(elem) {
-      var docViewTop = $(window).scrollTop();
-      var docViewBottom = docViewTop + $(window).height();
-      var elemTop = $(elem).offset().top;
-      return elemTop <= docViewBottom && elemTop >= docViewTop;
-    }
-
-    if ($(".skill-grid").length > 0) {
-      if (elementScrolled(".skill-grid")) {
-        $(".skill-grid .skill").each(function (i) {
-          var that = this;
-          var t = setTimeout(function () {
-            $(that).addClass("active");
-          }, 250 * i);
-        });
-      }
-    }
-
-    if ($("#post-content").length > 0) {
-      if ($(window).scrollTop() > $("#post-content").offset().top) {
-        $(".blog-topbar").addClass("active");
-      } else {
-        $(".blog-topbar").removeClass("active");
-      }
-    }
-  });
 });
 //Get params 
 function getParams(name) {
@@ -70,3 +22,29 @@ function getParams(name) {
   });
   return params[name];
 }
+//slide toggle
+
+const toggleLink = document.querySelectorAll('.toggle-link'); 
+toggleLink.forEach(el => {
+  el.addEventListener('click', function(event){
+  event.preventDefault();
+  this.classList.toggle('toggle-open')
+  const toggleBox = this.nextElementSibling;
+  if (!toggleBox.classList.contains('active')) {
+    toggleBox.classList.add('active');
+    toggleBox.style.height = 'auto';
+    const height = toggleBox.clientHeight + 'px';
+    toggleBox.style.height = '0px';
+    setTimeout(function () {
+      toggleBox.style.height = height;
+    }, 0);
+  } else {
+    toggleBox.style.height = '0px';
+    toggleBox.addEventListener('transitionend', event => {
+      toggleBox.classList.remove('active');
+    }, {
+      once: true
+    });
+  }
+  });
+});
